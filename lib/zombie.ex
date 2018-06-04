2d4e5d7349ef5588d6552fa6e86f1740e6e02ab2
Khaja Minhajuddin <minhajuddink@gmail.com>
2018-06-04 15:07:36 +0530

    Healthy zombie

--- lib/zombie.ex
+++ lib/zombie.ex
@@ -1,18 +1,47 @@
 defmodule Zombie do
   @moduledoc """
-  Documentation for Zombie.
+  Bring your variables back from the dead!
+  TODO: link to blog post
   """
+
+  @iex_state_filename ".iex_state"
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Zombie.hello
-      :world
-
+  Use this to save all your variables in the current scope to a file, so that they
+  can be reloaded using `Zombify.resurrect`
   """
-  def hello do
-    :world
+  def bury(bindings) do
+    File.write!(
+      state_filename(),
+      :erlang.term_to_binary(bindings)
+    )
+  end
+
+  @doc """
+  Use this to resurrect your saved variables
+  """
+  defmacro resurrect do
+    vars =
+      File.read!(state_filename())
+      |> :erlang.binary_to_term()
+
+    defs =
+      for {k, v} <- vars do
+        v = Macro.escape(v)
+
+        quote do
+          def unquote(k)(), do: unquote(v)
+        end
+      end
+
+    quote do
+      defmodule ZombieState do
+        unquote(defs)
+      end
+    end
+  end
+
+  defp state_filename do
+    Path.relative_to_cwd(@iex_state_filename)
   end
 end
--- test/zombie_test.exs
+++ test/zombie_test.exs
@@ -1,8 +1,12 @@
 defmodule ZombieTest do
   use ExUnit.Case
-  doctest Zombie
+  require Zombie
 
-  test "greets the world" do
-    assert Zombie.hello() == :world
+  test "zombie" do
+    File.rm_rf(".iex_state")
+    son = %{name: "Mujju", age: 3, likes: [:legos, "greenery", :slides]}
+    Zombie.bury(binding())
+    Zombie.resurrect()
+    assert ZombieState.son() == son
   end
 end
